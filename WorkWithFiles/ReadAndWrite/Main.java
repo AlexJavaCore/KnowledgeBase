5da5c4bc497a996e1850b99e261a9e7c9e833aab
Alexander Smolin <alexjavacore@mail.ru>
2022-11-19 15:41:39 +0300

    Add files via upload

---
+++ WorkWithFiles/ReadAndWrite/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        String outputFileName = "file.txt";
+        String[] array = {"one", "two", "three", "four"};
+
+        try (BufferedWriter writter = new BufferedWriter(new FileWriter(outputFileName))) {
+            for (String value : array) {
+                writter.write(value + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
