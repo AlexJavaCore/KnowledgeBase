e0fdc5a70a83d265c21e1901ccea7b4dfb29ffc5
Alexander Smolin <alexjavacore@mail.ru>
2023-01-28 13:02:51 +0300

    DateFormatting

--- WorkWithDate/DateFormatting.java
+++ WorkWithDate/DateFormatting.java
@@ -5,16 +5,29 @@ import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class DateFormat {
+public class DateFormatting {
     public static void main(String[] args) throws Exception {
+        // current date
         Date date = new Date();
         System.out.println(date);
+
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
+
+        // add one week
         calendar.add(Calendar.WEEK_OF_MONTH, 1);
         System.out.println(calendar.getTime());
 
+        // short date
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
         System.out.println(dateFormat.format(calendar.getTime()));
+
+        // date formatting
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        System.out.println(simpleDateFormat.format(calendar.getTime()));
+
+        // get date and parse it
+        Date newDate = simpleDateFormat.parse("05/12/1963 05:22");
+        System.out.println(newDate);
     }
 }
