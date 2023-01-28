47fb9a5692c685d6fa704f26f000d117c5c997d2
Alexander Smolin <alexjavacore@mail.ru>
2023-01-28 12:41:18 +0300

    DateFormat

--- WorkWithDate/DateFormat.java
+++ WorkWithDate/DateFormat.java
@@ -5,7 +5,7 @@ import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class SimpleDateFormat {
+public class DateFormat {
     public static void main(String[] args) throws Exception {
         Date date = new Date();
         System.out.println(date);
